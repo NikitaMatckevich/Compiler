03f3bd9217c44ccb464e9e1d2811f57f3994a398
NikitaMatckevich <57538710+NikitaMatckevich@users.noreply.github.com>
2020-10-22 22:09:05 +0200

    Add directory tree

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,19 +4,11 @@
 using namespace std;
 
 int main () { try {
-  Lexer L("../program.txt");
-  for (auto token : L.tokens) {
-    cout << "line " << token.line_number << " " << token.context;
-    if (token.is<Number>())
-      cout << " {" << token.as<Number>().value << "}";
-    cout << endl;
-  }
-  
+  Lexer L("../example.txt");
   size_t pos = 0;
   auto ptr = readProgram(L, pos);
   int code = ptr->exec();
   cout << "Program returned code " << code << endl;  
-
   return 0;
 }
 
